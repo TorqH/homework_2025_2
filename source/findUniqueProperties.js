7d753c054d227abd33c036a4725bdd002aa6a7e8
Daria <dari.sokolova@vkteam.ru>
2025-09-17 15:58:51 +0300

    поменяла дублирующиеся файлы с тестами на файлы функций

--- source/findUniqueProperties.js
+++ source/findUniqueProperties.js
@@ -1,47 +1,30 @@
 'use strict';
+/**
+ * Вариант 18
+ * Напишите функцию findUniqueProperties, которая принимает два объекта и возвращает новый объект, 
+ * содержащий только те свойства, которые уникальны для каждого из объектов.
+ * Уникальные свойства — это те, которые присутствуют в одном объекте, но отсутствуют в другом.
+ */
 
-QUnit.module("Тестируем функцию findUniqueProperties", function() {
-    QUnit.test("Работает правильно для объектов с уникальными свойствами", function(assert) {
-        const result = findUniqueProperties(
-            { a: 1, b: 2, c: 3 },
-            { b: 2, c: 4, d: 5 }
-        );
+/**
+ * Функция, которая принимает два объекта и возвращает новый объект, 
+ * содержащий только те свойства, которые уникальны для каждого из объектов.
+ * @param object1 - первый объект для сравнения
+ * @param object2 - второй объект для сравнения
+ * @example
+ * findUniqueProperties({ x: 10, y: 20 }, { y: 20, z: 30 })
+ * // returns 
+ * { x: 10, z: 30 }
+ * @returns {together}
+ */
+function findUniqueProperties(object1, object2) {
+  const together = { ...object1, ...object2 };
+  for (let key in together) {
+    if (key in object1 && key in object2) {
+      Reflect.deleteProperty(together, key);
+    }
+  }
+  return together;
+}
 
-        assert.deepEqual(result, { a: 1, d: 5 }, "Должны быть уникальные свойства из обоих объектов.");
-    });
-
-    QUnit.test("Работает правильно для объекты с отсутствующими свойствами", function(assert) {
-        const result = findUniqueProperties(
-            {},
-            {}
-        );
-
-        assert.deepEqual(result, {}, "Должны быть уникальные свойства x и z.");
-    });
-
-    QUnit.test("Работает правильно для идентичных объектов", function(assert) {
-        const result = findUniqueProperties(
-            { a: 1, b: 2 },
-            { a: 1, b: 2 }
-        );
-
-        assert.deepEqual(result, {}, "Идентичные объекты должны вернуть пустой объект.");
-    });
-
-    QUnit.test("Работает правильно если в объектах неодинаковое количество свойств", function(assert) {
-        const result = findUniqueProperties(
-            { x: 10, y: 20 },
-            { y: 20, z: 30, po: 0, yu: 11}
-        );
-
-        assert.deepEqual(result, {x: 10, z: 30, po: 0, yu: 11});
-    });
-
-    QUnit.test("Работает правильно если в объектах свойства разных типов", function(assert) {
-        const result = findUniqueProperties(
-            { x: 'a', y: 'A' },
-            { h: 20, z: '20'}
-        );
-        assert.deepEqual(result, {x: 'a', y: 'A', h: 20, z: '20'});
-    });
-});
+console.log(findUniqueProperties({ x: 'a', y: 'A' }, { h: 20, z: '20'}))
